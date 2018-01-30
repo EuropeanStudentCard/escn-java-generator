cb26b6e99f78766e9bcbbf1c4e255b2e185dff52
Geoffroy Vibrac <geoffroy.vibrac@gmail.com>
2018-01-30 08:52:14 +0100

    refactor test package name

--- src/test/java/eu/europeanstudentcard/esc/EscnFactoryTest.java
+++ src/test/java/eu/europeanstudentcard/esc/EscnFactoryTest.java
@@ -1,4 +1,4 @@
-package com.cnous.esc;
+package eu.europeanstudentcard.esc;
 
 import org.junit.Test;
 
@@ -8,22 +8,22 @@ import java.util.regex.Pattern;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class UuidFactoryTest {
+public class EscnFactoryTest {
     @Test
     public void getUuid() throws Exception {
         String uuidFormat = "(\\w{8}(-\\w{4}){3}-\\w{12}?)";
         Pattern p = Pattern.compile(uuidFormat);
         String returnTest;
 
-        returnTest = UuidFactory.getUuid(66, "999859608");
+        returnTest = EscnFactory.getEscn(66, "999859608");
 
         Matcher m = p.matcher(returnTest);
         assertTrue(m.matches());
         assertEquals("066999859608", returnTest.substring(returnTest.length() - 12));
     }
 
-    @Test(expected = UuidFactoryException.class)
+    @Test(expected = EscnFactoryException.class)
     public void testIllegalArgumentException() throws Exception {
-        UuidFactory.getUuid(6666, "999859608");
+        EscnFactory.getEscn(6666, "999859608");
     }
 }
